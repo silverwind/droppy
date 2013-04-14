b14be46c17a5355db7badddf125f2dc38cd7320d
silverwind <silv3rwind@gmail.com>
2013-04-14 04:55:10 +0200

    Simplfy startup scripts

--- droppy.bat
+++ droppy.bat
@@ -5,33 +5,4 @@ echo ====================
 echo Droppy - Fileserver
 echo ====================
 echo.
-echo ### Checking dependancies...
-call :FirstCheck
-
-:FirstCheck
-npm list | findstr ws@ > NUL
-if %ERRORLEVEL%==1 (
-    echo ### Installing socket.io
-    npm install socket.io
-    call :SecondCheck
-)
-
-:SecondCheck
-npm list | findstr mime@ > NUL
-if %ERRORLEVEL%==1 (
-    echo ### Installing mime
-    npm install mime
-    call :ThirdCheck
-)
-
-:ThirdCheck
-npm list | findstr formidable@ > NUL
-if %ERRORLEVEL%==1 (
-    echo ### Installing formidable
-    npm install formidable@latest
-    call :Run
-)
-
-:Run
-echo ### Droppy: starting up...
-node droppy
+npm install && cls && node droppy
--- droppy.sh
+++ droppy.sh
@@ -4,29 +4,11 @@ echo "===================="
 echo "Droppy - Fileserver"
 echo "===================="
 echo ""
-echo ">>> Checking dependancies..."
-npm list  | grep ws@ > /dev/null
-if [ $? -eq 1 ]
-then
-	echo ">>> Installing ws"
-	npm install ws@latest
-fi
 
-npm list | grep mime@ > /dev/null
-if [ $? -eq 1 ]
-then
-	echo ">>> Installing mime"
-	npm install mime@latest
+npm install
+if [ $? != 0 ]; then
+    exit $?
 fi
-
-npm list | grep formidable@ > /dev/null
-if [ $? -eq 1 ]
-then
-	echo ">>> Installing formidable"
-	npm install formidable@latest
-fi
-
-echo ">>> Starting up..."
 
 # If a cygwin enviroment is detected, run it with cmd
 if [ `uname -o` = "Cygwin" ]
@@ -35,3 +17,5 @@ then
 else
     node droppy
 fi
+
+
