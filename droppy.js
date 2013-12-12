4804f69f793b496a0e924f15395d80086d1a7ce4
silverwind <silv3rwind@gmail.com>
2013-12-12 19:06:23 +0100

    Remove outdated TODO section

--- droppy.js
+++ droppy.js
@@ -22,17 +22,6 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
- ------------------------------------------------------------------------------
-  TODOs:
-  - Improve perceived responsivness through preloading/caching
-  - Add  a interface to add/remove users
-  - Improve performance of CSS transitions
-  - Drag and drop to move/copy entries
-  - Image thumbnails
-  - Keyboard navigation
-  - SVG icons
-  - Rework client <-> server communication
-  - Modularize client and server javascript code more
  --------------------------------------------------------------------------- */
 "use strict";
 
