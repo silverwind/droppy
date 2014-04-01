542b2cc7a4876b7a5a11b960fb8aed43acc4f3a0
silverwind <me@silverwind.io>
2014-04-01 21:32:47 +0200

    Add a basename() call, just to be sure

--- src/client.js
+++ src/client.js
@@ -1814,7 +1814,7 @@
 
     function openFile(view) {
         // Determine filetype and how to open it
-        var ext = getExt(getViewLocation(view));
+        var ext = getExt(basename(getViewLocation(view)));
         if (["png", "jpg", "gif", "bmp", "apng"].indexOf(ext !== -1)) {
             openImage(view);
         } else {
