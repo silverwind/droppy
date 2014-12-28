2390f1ce69b64b121a42c6ec78a7ab2eaf9f82c9
silverwind <me@silverwind.io>
2014-12-28 22:30:16 +0100

    Disable minifyCSS in HTML

--- server/lib/caching.js
+++ server/lib/caching.js
@@ -45,7 +45,7 @@ var minfierOptions = {
             collapseBooleanAttributes: true,
             removeRedundantAttributes: true,
             caseSensitive: true,
-            minifyCSS: minfierOptions.cleanCSS
+            minifyCSS: false
         };
     }
 };
