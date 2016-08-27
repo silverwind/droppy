d991ee00001635c6fa8cd4a9e8226c60cf30a90a
silverwind <me@silverwind.io>
2016-08-27 15:26:03 +0200

    add missing svg.js file

---
+++ server/svg.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var svgstore = require("svgstore");
+var fs = require("fs");
+var path = require("path");
+var paths = require("./paths.js").get();
+
+module.exports = function spritesheet() {
+  var sprites = svgstore();
+
+  fs.readdirSync(paths.svg).forEach(function(file) {
+    sprites.add("i-" + file.replace(/\.svg/, ""), fs.readFileSync(path.join(paths.svg, file)));
+  });
+
+  return sprites.toString({inline: true});
+};
+
