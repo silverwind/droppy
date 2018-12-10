b31c20b11e6abc6918536fc89b02ed6c4e098cb3
silverwind <me@silverwind.io>
2018-12-10 22:31:31 +0100

    Add Cmd-A and Ctrl-Space editor key bindings

--- client/client.js
+++ client/client.js
@@ -2034,6 +2034,10 @@
         // Let Mod-T through to the browser
         CodeMirror.keyMap.sublime["Cmd-T"] = false;
         CodeMirror.keyMap.sublime["Ctrl-T"] = false;
+
+        CodeMirror.keyMap.sublime["Cmd-A"] = "selectAll";
+        CodeMirror.keyMap.sublime["Ctrl-A"] = "selectAll";
+        CodeMirror.keyMap.sublime["Cmd-Space"] = "toggleCommentIndented";
         CodeMirror.keyMap.sublime["Ctrl-Space"] = "toggleCommentIndented";
 
         view[0].lineEnding = dominantLineEnding(text);
