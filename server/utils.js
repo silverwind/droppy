9418a9c3c2eed30f063295ebea3d5108268640c4
jed <inmysocks@fastmail.com>
2019-02-09 23:06:21 +0100

    Add initial support for applications using body-parser
    If the express app is using body-parser than utils.readJsonBody would fail, this handles the case when body-parser is used and the body is already a json object.

--- server/utils.js
+++ server/utils.js
@@ -217,19 +217,34 @@ utils.createSid = function() {
 };
 
 utils.readJsonBody = function(req) {
-  return new Promise(((resolve, reject) => {
-    let body = [];
-    req.on("data", chunk => {
-      body.push(chunk);
-    }).on("end", () => {
-      body = String(Buffer.concat(body));
+  if (req.body) {
+    // This is needed if the express application is using body-parser
+    return new Promise(((resolve, reject) => {
       try {
-        resolve(JSON.parse(body));
+        if (typeof req.body === 'object') {
+          resolve(req.body);
+        } else {
+          resolve(JSON.parse(req.body))
+        }
       } catch (err) {
         reject(err);
       }
-    });
-  }));
+    }));
+  } else {
+    return new Promise(((resolve, reject) => {
+      let body = [];
+      req.on("data", chunk => {
+        body.push(chunk);
+      }).on("end", () => {
+        body = String(Buffer.concat(body));
+        try {
+          resolve(JSON.parse(body));
+        } catch (err) {
+          reject(err);
+        }
+      });
+    }));
+  }
 };
 
 utils.countOccurences = function(string, search) {
